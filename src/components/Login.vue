bd0a9946046c408e7d7b6f77b50912e11efb27b6
ramarathore177 <ramarathore177@gmail.com>
2022-02-17 18:50:21 +0530

    Adding master branch

---
+++ src/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="5">
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field
+            v-model="name"
+            :rules="nameRules"
+            label="Name"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+
+          <!-- <v-checkbox
+                v-model="checkbox"
+                :rules="[v => !!v || 'You must agree to continue!']"
+                label="Do you agree?"
+                required
+                ></v-checkbox> -->
+
+          <v-btn color="success" class="mr-4" @click="submit"> Validate </v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
